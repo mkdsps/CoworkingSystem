7a422cc11c71377693767e0560a094693ab3a2cf
Igor Ivanovic <igor.ivanovic.ne@gmail.com>
2026-02-09 18:25:30 +0100

    pocetak dodavanja repoFactori-ja

---
+++ CoworkingSystem/backend/Repositories/IKorisniciRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoworkingSystem.backend.Repositories
+{
+    interface IKorisniciRepo
+    {
+        public void InsertUser()
+    }
+}
---
+++ CoworkingSystem/backend/Repositories/IRezervacijeRepo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoworkingSystem.backend.Repositories
+{
+    interface IRezervacijeRepo
+    {
+    }
+}
