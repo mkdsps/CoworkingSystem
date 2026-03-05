3116bedfa776e07ae2645d85b45d38c6ebe54cd2
Igor Ivanovic <igor.ivanovic.ne@gmail.com>
2026-03-05 12:55:20 +0100

    Runnners dodani kako bi mogli odvojeno da testiramo...

---
+++ CoworkingSystem/backend/Runners/Igor.cs
@@ -0,0 +1,39 @@
+﻿using CoworkingSystem.backend.Modules;
+using CoworkingSystem.backend.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoworkingSystem.backend.Runners
+{
+    internal class Igor
+    {
+        public static void Run()
+        {
+            try
+            {
+                var repo = new SqlKorisnikRepo();
+                var k = new Korisnik
+                {
+                    Ime = "Igor",
+                    Prezime = "Test",
+                    Email = $"igor_{DateTime.Now:yyyyMMdd_HHmmss}@mail.com",
+                    Telefon = "060000000",
+                    TipClanstvaId = 1,
+                    DatumPocetka = DateTime.Now,
+                    DatumIsteka = DateTime.Now.AddDays(30),
+                    Status = "aktivan",
+                    LokacijaId = 1,
+                    Napomena = "igor insert"
+                };
+
+                repo.InsertUser(k);
+                MessageBox.Show("IGOR INSERT OK");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "IGOR FAIL");
+            }
+        }
+    }
+}
