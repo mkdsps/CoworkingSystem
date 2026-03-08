fcf38d99bb347344faa35011d08657cde78e7af4
iliccandrija <ilicandrija1@gmail.com>
2026-03-08 11:06:10 +0100

    Dodat kostur za Izvestaj

---
+++ CoworkingSystem/backend/Services/StatistikaService.cs
@@ -0,0 +1,53 @@
+﻿using CoworkingSystem.backend.Repositories;
+using CoworkingSystem.backend.Statistike;
+using System;
+using System.Collections.Generic;
+
+namespace CoworkingSystem.backend.Services
+{
+    internal class StatistikaService
+    {
+        private readonly IResursiRepo _resursiRepo;
+        private readonly ITipClanstvaRepo _tipClanstvaRepo;
+
+        public StatistikaService(IResursiRepo resursiRepo, ITipClanstvaRepo tipClanstvaRepo)
+        {
+            _resursiRepo = resursiRepo;
+            _tipClanstvaRepo = tipClanstvaRepo;
+        }
+
+        public List<RadnoMestoStatistika> GetStatistikaRadnihMesta(DateTime periodOd, DateTime periodDo)
+        {
+            ValidatePeriod(periodOd, periodDo);
+
+            var rezultat = new List<RadnoMestoStatistika>();
+
+
+            return rezultat;
+        }
+
+        public List<SalaStatistika> GetStatistikaSala(DateTime periodOd, DateTime periodDo)
+        {
+            ValidatePeriod(periodOd, periodDo);
+
+            var rezultat = new List<SalaStatistika>();
+
+            return rezultat;
+        }
+
+        public List<TipClanstvaStatistika> GetStatistikaTipovaClanstva(DateTime periodOd, DateTime periodDo)
+        {
+            ValidatePeriod(periodOd, periodDo);
+
+            var rezultat = new List<TipClanstvaStatistika>();
+
+            return rezultat;
+        }
+
+        private void ValidatePeriod(DateTime periodOd, DateTime periodDo)
+        {
+            if (periodOd >= periodDo)
+                throw new Exception("Pocetni datum perioda mora biti manji od krajnjeg datuma.");
+        }
+    }
+}
---
+++ CoworkingSystem/backend/Statistike/SalaStatistika.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoworkingSystem.backend.Statistike
+{
+    public class SalaStatistika
+    {
+        public int ResursId { get; set; }
+        public string Oznaka { get; set; }
+        public int LokacijaId { get; set; }
+        public string NazivLokacije { get; set; }
+
+        public int Kapacitet { get; set; }
+
+        public int UkupanBrojRezervacija { get; set; }
+        public double UkupnoSatiKoriscenja { get; set; }
+        public double ProsecnoTrajanjeRezervacijeSati { get; set; }
+        public double ProcenatZauzetosti { get; set; }
+
+        public int UkupanBrojUcesnika { get; set; }
+        public double ProsecanBrojUcesnika { get; set; }
+        public int BrojRazlicitihKorisnika { get; set; }
+    }
+}
